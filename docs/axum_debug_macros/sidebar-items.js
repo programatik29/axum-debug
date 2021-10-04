e0925586929788a7cb2828981c4d6a51c20ddbac
Programatik <programatik29@gmail.com>
2021-10-04 17:36:26 +0300

    update docs

--- docs/axum_debug_macros/sidebar-items.js
+++ docs/axum_debug_macros/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({});
+initSidebarItems({"attr":[["debug_handler","Generates better error messages when applied to a handler function."]],"macro":[["debug_router","Shortens error message when applied to a `Router`."]]});
--- docs/search-index.js
+++ docs/search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"axum_debug":{"doc":"","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]},\
-"axum_debug_macros":{"doc":"","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]}\
+"axum_debug":{"doc":"This is a debugging crate that provides better error …","t":[23,14],"n":["debug_handler","debug_router"],"q":["axum_debug",""],"d":["Generates better error messages when applied to a handler …","Shortens error message when applied to a <code>Router</code>."],"i":[0,0],"f":[null,null],"p":[]},\
+"axum_debug_macros":{"doc":"Procedural macros for <code>axum-debug</code> crate.","t":[23,14],"n":["debug_handler","debug_router"],"q":["axum_debug_macros",""],"d":["Generates better error messages when applied to a handler …","Shortens error message when applied to a <code>Router</code>."],"i":[0,0],"f":[null,null],"p":[]}\
 }');
 if (window.initSearch) {window.initSearch(searchIndex)};
